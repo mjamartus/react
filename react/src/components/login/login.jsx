56c18188bca17b033027d949490e728b1f4c854d
Mateusz Juraniec <mateuszjuraniec@AMARTUS0006.localdomain>
2023-05-22 11:05:38 +0200

    login and register pass to backend

--- react/src/components/login/login.jsx
+++ react/src/components/login/login.jsx
@@ -1,16 +1,52 @@
 import React from "react";
 import "./login.css"
 
-const Login = ({ onRouteChange }) => {
-    return (
-        <div className="cover">
-            <h1>Login</h1>
-            <input type="text" placeholder="username" />
-            <input type="password" placeholder="password" />
-            <div className="login-btn" onClick={() => onRouteChange('successLogin')}>LOGIN</div>
-            <div className="register-btn" onClick={() => onRouteChange('register')}>REGISTER</div>
-        </div>
-    )
+class Login extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            loginUsername: '',
+            loginPassword: ''
+        }
+    }
+
+    onUsernameChange = (event) => {
+        this.setState({ loginUsername: event.target.value })
+    }
+
+    onPasswordChange = (event) => {
+        this.setState({ loginPassword: event.target.value })
+    }
+
+    onSummitLogin = () => {
+        fetch('http://localhost:3000/login', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                username: this.state.loginUsername,
+                password: this.state.loginPassword
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data === 'success') {
+                    this.props.onRouteChange('successLogin')
+                }
+            })
+    }
+
+    render() {
+        const { onRouteChange } = this.props
+        return (
+            <div className="cover">
+                <h1>Login</h1>
+                <input type="text" placeholder="username" onChange={this.onUsernameChange} />
+                <input type="password" placeholder="password" onChange={this.onPasswordChange} />
+                <div className="login-btn" onClick={this.onSummitLogin}>LOGIN</div>
+                <div className="register-btn" onClick={() => onRouteChange('register')}>REGISTER</div>
+            </div>
+        )
+    }
 }
 
 export default Login
--- react/src/components/register/register.jsx
+++ react/src/components/register/register.jsx
@@ -1,15 +1,51 @@
 import React from "react";
 import "./register.css"
 
-const Register = ({ onRouteChange }) => {
-    return (
-        <div className="cover">
-            <h1>Register</h1>
-            <input type="text" placeholder="username" />
-            <input type="password" placeholder="password" />
-            <div className="reg-btn" onClick={() => onRouteChange('successRegister')}>REGISTER</div>
-        </div>
-    )
+class Register extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    onUsernameChange = (event) => {
+        this.setState({ username: event.target.value })
+    }
+
+    onPasswordChange = (event) => {
+        this.setState({ password: event.target.value })
+    }
+
+    onSummitRegister = () => {
+        fetch('http://localhost:3000/register', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                username: this.state.username,
+                password: this.state.password
+            })
+        })
+            .then(response => response.json())
+            .then(user => {
+                if (user) {
+                    this.props.onRouteChange('successRegister')
+                }
+            })
+    }
+
+    render() {
+        const { onRouteChange } = this.props
+        return (
+            <div className="cover">
+                <h1>Register</h1>
+                <input type="text" placeholder="username" onChange={this.onUsernameChange} />
+                <input type="password" placeholder="password" onChange={this.onPasswordChange} />
+                <div className="reg-btn" onClick={this.onSummitRegister}>REGISTER</div>
+            </div>
+        )
+    }
 }
 
 export default Register
